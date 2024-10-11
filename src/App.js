0e434d3b75635178639ce9ebe3fe34063a51e2b2
Angel D <20071381+angeldeniz11235@users.noreply.github.com>
2024-10-11 13:30:47 -0700

    Refactor API service to fetch and set background image URLs
    - Remove .env file
    - Update App component to fetch and set background image URLs from the API
    - Add new function getImageUrls to retrieve image URLs from a specific collection and field in the API service
    - Set the background image URL state in the App component to the 'large' format URL

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import { fetchHomePageURL } from './services/api';
+import { fetchHomePageURL, getImageUrls } from './services/api';
 
 function App() {
   const [homepageURL, setHomepageURL] = useState('');
+  const [backgroundImgURL, setBackgroundImgURL] = useState(null);
 
   useEffect(() => {
+    // Fetch the homepage URL
     fetchHomePageURL()
       .then((url) => {
         setHomepageURL(url);
@@ -12,10 +14,26 @@ function App() {
       .catch((error) => {
         console.error('Error fetching homepage URL:', error);
       });
+
+    // Fetch the background image URLs
+    (async () => {
+      const bgImgURLs = await getImageUrls('home-summary', 'Logo');
+      console.log(bgImgURLs);
+      // Set the background image URL state to the 'large' format
+      console.log(bgImgURLs?.large)
+      setBackgroundImgURL(bgImgURLs?.large);
+    })();
   }, []);
 
   return (
-    <div>
+    <div 
+      style={{ 
+        backgroundImage: `url(${backgroundImgURL})`, 
+        backgroundSize: 'cover', 
+        backgroundPosition: 'center', 
+        height: '100vh' 
+      }}
+    >
       <h1>My Website</h1>
       <p>Homepage URL: {homepageURL}</p>
     </div>
--- src/services/api.js
+++ src/services/api.js
@@ -9,4 +9,30 @@ export const fetchHomePageURL = async () => {
   return response.data.data.attributes.homepage_url;
 }
 
+// Function to retrieve image URLs from a specific collection and field
+export async function getImageUrls(collectionName, fieldName) {
+  try {
+    const response = await axios.get(`${process.env.REACT_APP_API_URL}${collectionName}?populate=${fieldName}`);
+
+    // Check if data exists
+    const imageData = response.data?.data?.attributes?.[fieldName]?.data?.attributes?.formats;
+
+    if (!imageData) {
+      return null;
+    }
+
+    // Create a hash map of formats and their URLs
+    const imageUrls = {};
+    for (const format in imageData) {
+      imageUrls[format] = `${process.env.REACT_APP_CMS_URL.replace(/\/$/, '')}${imageData[format].url}`;
+    }
+
+    return imageUrls;
+  } catch (error) {
+    console.error('Error fetching image URLs:', error);
+    return null;
+  }
+}
+
+
 export default api;
